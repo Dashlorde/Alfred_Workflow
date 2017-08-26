5594051059608d5c280bbe7d8bbfde2f6d76327c
allrainvot <zhouyunlu0216@gmail.com>
2017-08-26 19:14:16 -0400

    separate function

--- douban/new_movie.py
+++ douban/new_movie.py
@@ -1,45 +1,26 @@
-# encoding: utf-8
-
 import sys
-import urllib
 from workflow import Workflow, web
 
-logger = None
 
-# search movies
-def search(query):
-
-    # 编码，支持中文
-    # query = urllib.quote(query.encode('utf8')).decode('utf8')
-
-    url = "https://api.douban.com/v2/movie/search?q=%s" % query
+# movies in theaters
+def new_movie():
+    url = "https://api.douban.com/v2/movie/in_theaters"
     r = web.get(url)
 
-    # throw error if request failed
     r.raise_for_status()
 
     movies = []
-
-    # 解析json，详情见https://developers.douban.com/wiki/?title=movie_v2#subject
     data = r.json()
     results = data['subjects']
 
     for movie in results:
         movies.append(movie)
-
     return movies
-
-
 
 
 def main(wf):
     
-    query = ' '.join(wf.args)
-    
-    def wrapper():
-        return search(query)
-    movies = wf.cached_data(query, wrapper, max_age=600)
-    
+    movies = wf.cached_data("new_movie", new_movie, max_age=600)
 
     for movie in movies:
         wf.add_item(uid=movie['id'],
